e44bdc1e310b36c04e0c44b1fdebc0b1ca88cf6b
Teo <teosany@icloud.com>
2024-02-22 11:56:25 +0100

    add blitz player

--- index.php
+++ index.php
@@ -96,6 +96,18 @@ class QueuingPlayer extends Player
         $this->range = min($this->range + 1, 40);
     }
 }
+final class BlitzPlayer extends Player
+{
+    public function __construct(public string $name = 'anonymous', public float $ratio = 1200)
+    {
+        parent::__construct($name, $ratio);
+    }
+    public function updateRatioAgainst(AbstractPlayer $player, int $result): void
+    {
+        $this->ratio += 128 * ($result - $this->probabilityAgainst($player));
+    }
+
+}
 
 $greg = new Player('greg');
 $jade = new Player('jade');
